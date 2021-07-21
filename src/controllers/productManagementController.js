0462676cac8af6d960009e28f41c01d138a98fd3
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-07-20 21:23:09 -0500

    Create products

--- src/controllers/productManagementController.js
+++ src/controllers/productManagementController.js
@@ -1,21 +1,39 @@
 const fs = require('fs');
 const path = require('path');
-const dataProduct = fs.readFileSync(path.join(__dirname,'../data/products.json'),'utf-8');
+const dataProduct = fs.readFileSync(path.join(__dirname, '../data/products.json'), 'utf-8');
 let products = JSON.parse(dataProduct);
 const controller = {
-    index: (req, res)=>{
+    index: (req, res) => {
         res.redirect('/');
     },
-    detailProduct: (req, res)=>{
+    detailProduct: (req, res) => {
         res.send('detalles no disponibles por el momento');
     },
-    createForm: (req, res)=>{
+    createForm: (req, res) => {
         res.render('./productManagement/createProduct');
     },
-    createProduct: (req, res)=>{
-        res.send('Creando producto')
+    createProduct: (req, res) => {
+        if (req.file) {
+            //console.log(req.file);
+            // console.log(req.body)
+            let product = {
+                id: products.length,
+                name: req.body.productName,
+                description: req.body.productDescription,
+                image: req.file.filename,
+                category: req.body.productCategory
+            }
+            products.push(product);
+
+            let productsJSON = JSON.stringify(products);
+            fs.writeFileSync(path.join(__dirname, '../data/products.json'),productsJSON);
+            res.redirect('/');
+        }
+        else{
+            res.redirect('/products/create');
+        }
     },
-    editProduct: (req, res)=>{
+    editProduct: (req, res) => {
         res.render('./productManagement/editProduct');
     }
 }
--- src/routes/productManagement.js
+++ src/routes/productManagement.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
 const router = express.Router();
 const productManagementController = require ('../controllers/productManagementController');
 
+var storage = multer.diskStorage({
+    destination : (req, file , cb) =>{
+        cb(null,path.join(__dirname, '../../public/images/products'));
+    },
+    filename : (req, file , cb)=>{
+        cb(null,'product_'+ Date.now() + path.extname(file.originalname));
+    }
+})
+
+var upload = multer ({ storage });
 
 router.get('/', productManagementController.index);              //1. Listado de productos
 router.get('/create', productManagementController.createForm);//2. Formulario de creación de productos
-router.post('/create', productManagementController.createProduct);
+router.post('/create',upload.single('productImage'), productManagementController.createProduct);
 
 router.get('/:id', productManagementController.detailProduct);   //3. Detalle de un producto particular
 router.post('/', productManagementController.index);             //4. Acción de creación
