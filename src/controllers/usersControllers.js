9eab86f15a5d2fb40e3191f906f590f395663636
Axel Manzanilla <axelmanzanilla@hotmail.com>
2021-08-09 17:58:14 -0500

    ya se pueden crear nuevos usuarios

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,7 +1,54 @@
+const fs = require('fs');
+const path = require('path');
+let users = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/users.json'), {encoding : 'utf-8'}));
 
 const controllers = {
-    index : (req,res) =>{
-        res.send('Entidad de usuarios')
+    index : (req, res) => {
+        // Si es un usuario se regresa al menu
+        // Si es admin te muestra la lista de usuarios
+        res.redirect('/')
+    },
+    createUser : (req, res) => {
+        if(req.body.terminos){
+            let newId = users[users.length - 1].id + 1;
+            let spam = (req.body.spam) ? "true" : "false";
+
+            let user = {
+                id: newId,
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                email: req.body.email,
+                password: req.body.password,
+                category: "user",
+                spam: spam,
+                image: "default.jpg"
+            }
+
+            users.push(user);
+
+            let usersJSON = JSON.stringify(users, null, 1);
+            fs.writeFileSync(path.join(__dirname, '../data/users.json'), usersJSON);
+            res.redirect('/login')
+        }
+        else{
+            res.redirect('/register');
+        }
+    },
+    detailUser : (req, res) => {
+        // Si es un usuario se regresa al menu
+        // Si es admin te muestra la lista de usuarios
+        res.send('detalles del usuario')
+    },
+    editUserForm : (req, res) => {
+        // Si es un usuario se regresa al menu
+        // Si es admin te muestra la lista de usuarios
+        res.render('form de edicion del usuario')
+    },
+    editUser : (req, res) => {
+        res.send('editar del usuario')
+    },
+    deleteUser : (req, res) => {
+        res.send('borrar del usuario')
     }
 };
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 const usersControllers = require('../controllers/usersControllers');
 
-router.get('/',usersControllers.index);
+router.get('/', usersControllers.index);                    // Listado de productos
+router.post('/', usersControllers.createUser);              // Acción de creación de usuario
+router.get('/:id', usersControllers.detailUser);            // Detalle de un usuario particular
+router.put('/:id', usersControllers.editUser);              // Acción de edición
+router.delete('/:id', usersControllers.deleteUser);         // Acción de borrado
+router.get('/edit/:id', usersControllers.editUserForm);     // Formulario de edición de usuario
 
 module.exports = router;
