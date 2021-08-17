99eae6f156f5d1d38f2bc3d5f69a31d3c99d4b97
Axel Manzanilla <axelmanzanilla@hotmail.com>
2021-08-17 13:44:24 -0500

    Se empezaron las correcciones

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const path = require ('path');
+
 const controller = {
-    index: (req, res)=>{
+    index: (req, res) => {
+        let productosAMostrar = 5;
         let productsJSON = fs.readFileSync(path.join(__dirname,'../data/products.json'), {encoding: "utf-8"});
         let products = JSON.parse(productsJSON);
 
-        res.render('home', {products: products});
+        res.render('home', {products: products, number: productosAMostrar});
     },
 }
 
