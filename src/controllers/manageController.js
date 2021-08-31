2f8ff14daec95285de60eb5e26ccace369ea7afc
Axel Manzanilla <axelmanzanilla@hotmail.com>
2021-08-31 17:32:30 -0500

    se creó la vista para visualizar los recibos

--- src/controllers/manageController.js
+++ src/controllers/manageController.js
@@ -1,8 +1,34 @@
 const db = require('../database/models');
 
 const controller = {
-    index: async (req, res) => {
+    index: (req, res) => {
         res.render('./manage/index');
+    },
+
+    checks: async (req, res) => { // YA
+        let checks = await db.Check.findAll({ include: [{ association: 'user' }, { association: 'products' }] });
+        res.render('./manage/checks', { checks });
+    },
+
+    shoppingCarts: async (req, res) => {
+        let shoppingcarts = await db.ShoppingCart.findAll({
+            include: [{ association: 'user'}]
+        });
+
+        res.send(shoppingcarts);
+    },
+
+    models: async (req, res) => {
+        let models = await db.Model.findAll();
+
+        res.send(models);
+    },
+
+    categories: async (req, res) => {
+        let productCategories = await db.ProductCategory.findAll();
+        let userCategories = await db.UserCategory.findAll();
+
+        res.send(userCategories);
     }
 }
 
--- src/routes/manage.js
+++ src/routes/manage.js
@@ -5,6 +5,11 @@ const manageUsers = require('../routes/manageUsers');
 const manageController = require ('../controllers/manageController');
 
 router.get('/', manageController.index);
+router.get('/checks', manageController.checks);
+router.get('/shoppingcarts', manageController.shoppingCarts);
+router.get('/models', manageController.models);
+router.get('/categories', manageController.categories);
+
 router.use('/products', manageProducts);
 router.use('/users', manageUsers);
 
