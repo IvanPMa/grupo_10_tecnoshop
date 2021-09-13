d4968032c21f7d8c06cc01733c8307116a54e30d
Axel Manzanilla <axelmanzanilla@hotmail.com>
2021-09-12 20:52:08 -0500

    se agregó la base de datos en heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -9,15 +9,15 @@ module.exports = {
   "test": {
     "username": "root",
     "password": null,
-    "database": "database_test",
+    "database": "tecnoshop",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "bd19789a75d916",
+    "password": '886f02aa',
+    "database": "heroku_b62674cb3f9e2bd",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   }
 }
