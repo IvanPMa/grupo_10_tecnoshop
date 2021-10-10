45e9dfad37c70c6790bb605822405dffda47cb34
Axel Manzanilla <axelmanzanilla@gmail.com>
2021-10-10 14:40:55 -0500

    se quito el redireccionamiento del darkmode

--- public/js/changeDarkMode.js
+++ public/js/changeDarkMode.js
@@ -9,14 +9,25 @@ window.addEventListener('load', function(){
             cssDarkMode.rel = 'stylesheet';
             cssDarkMode.href = '/css/partials/darkmode.css';
             head.appendChild(cssDarkMode);
-            fetch('https://tecnoshop2.herokuapp.com/darkmode/1');
+
+            fetch('https://tecnoshop2.herokuapp.com/darkmode/1', {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => console.log(response))
+                .catch(error => console.log(error))
         }
         else{
             // Quitar el css que vuelve oscuro la página
             let links  = Array.from(document.getElementsByTagName('link'));
             let cssDarkMode = links.find(l => l.href.includes('/css/partials/darkmode.css'));
             head.removeChild(cssDarkMode);
-            fetch('https://tecnoshop2.herokuapp.com/darkmode/0');
+            fetch('http://localhost:3000/darkmode/0')
+                .then(response => console.log(response))
+                .catch(error => console.log(error))
         }
     });
 });
