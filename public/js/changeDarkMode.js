4fc7308a60872b6f8aa5ed9f0fae1ee9eaf2cfb0
Axel Manzanilla <axelmanzanilla@gmail.com>
2021-10-10 14:44:18 -0500

    se actualizó el fetch

--- public/js/changeDarkMode.js
+++ public/js/changeDarkMode.js
@@ -10,24 +10,19 @@ window.addEventListener('load', function(){
             cssDarkMode.href = '/css/partials/darkmode.css';
             head.appendChild(cssDarkMode);
 
-            fetch('https://tecnoshop2.herokuapp.com/darkmode/1', {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(response => console.log(response))
-                .catch(error => console.log(error))
+            // Mandar la información al servidor
+            fetch('https://tecnoshop.herokuapp.com/darkmode/1')
+                .catch(error => console.log(error));
         }
         else{
             // Quitar el css que vuelve oscuro la página
             let links  = Array.from(document.getElementsByTagName('link'));
             let cssDarkMode = links.find(l => l.href.includes('/css/partials/darkmode.css'));
             head.removeChild(cssDarkMode);
-            fetch('http://localhost:3000/darkmode/0')
-                .then(response => console.log(response))
-                .catch(error => console.log(error))
+
+            // Mandar la información al servidor
+            fetch('https://tecnoshop.herokuapp.com/darkmode/1')
+                .catch(error => console.log(error));
         }
     });
 });
