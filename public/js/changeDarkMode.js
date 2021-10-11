59f1436893b466688676ab3067aaea73f277738a
Axel Manzanilla <axelmanzanilla@gmail.com>
2021-10-10 19:59:35 -0500

    se cambio el logo a blanco

--- public/js/changeDarkMode.js
+++ public/js/changeDarkMode.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', function(){
     const darkMode = document.getElementById('darkmode');
     const head = document.getElementsByTagName('head')[0];
+    const logo = document.querySelector('.logo');
+    const search = document.querySelector('.search-img');
 
     darkMode.addEventListener('change', function(){
         if(darkMode.checked){
@@ -9,10 +11,11 @@ window.addEventListener('load', function(){
             cssDarkMode.rel = 'stylesheet';
             cssDarkMode.href = '/css/partials/darkmode.css';
             head.appendChild(cssDarkMode);
+            logo.src = '/images/logodark.png';
+            search.src = '/images/icons/searchwhite.png';
 
             // Mandar la información al servidor
             fetch('https://tecnoshop2.herokuapp.com/darkmode/1')
-                .then(res => console.log(res))
                 .catch(error => console.log(error));
         }
         else{
@@ -20,6 +23,8 @@ window.addEventListener('load', function(){
             let links  = Array.from(document.getElementsByTagName('link'));
             let cssDarkMode = links.find(l => l.href.includes('/css/partials/darkmode.css'));
             head.removeChild(cssDarkMode);
+            logo.src = '/images/logo.png';
+            search.src = '/images/icons/search.png';
 
             // Mandar la información al servidor
             fetch('https://tecnoshop2.herokuapp.com/darkmode/0')
