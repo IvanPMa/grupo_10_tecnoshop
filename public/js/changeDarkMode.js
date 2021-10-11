4c053333a939e60a0cfbce3e7492b6c46e8d58f5
Axel Manzanilla <axelmanzanilla@gmail.com>
2021-10-10 20:05:29 -0500

    se arregló un problema con el estilo oscuro

--- public/js/changeDarkMode.js
+++ public/js/changeDarkMode.js
@@ -1,34 +1,38 @@
 window.addEventListener('load', function(){
     const darkMode = document.getElementById('darkmode');
     const head = document.getElementsByTagName('head')[0];
+    const links  = Array.from(document.getElementsByTagName('link'));
+    const cssDarkMode = links.find(l => l.href.includes('/css/partials/darkmode.css'));
     const logo = document.querySelector('.logo');
     const search = document.querySelector('.search-img');
 
     darkMode.addEventListener('change', function(){
         if(darkMode.checked){
             // Agregar el css que vuelve oscuro la página
-            let cssDarkMode = document.createElement('link');
-            cssDarkMode.rel = 'stylesheet';
-            cssDarkMode.href = '/css/partials/darkmode.css';
-            head.appendChild(cssDarkMode);
-            logo.src = '/images/logodark.png';
-            search.src = '/images/icons/searchwhite.png';
-
-            // Mandar la información al servidor
-            fetch('https://tecnoshop2.herokuapp.com/darkmode/1')
-                .catch(error => console.log(error));
+            if(!cssDarkMode){
+                let addCssDarkMode = document.createElement('link');
+                addCssDarkMode.rel = 'stylesheet';
+                addCssDarkMode.href = '/css/partials/darkmode.css';
+                head.appendChild(addCssDarkMode);
+                logo.src = '/images/logodark.png';
+                search.src = '/images/icons/searchwhite.png';
+    
+                // Mandar la información al servidor
+                fetch('https://tecnoshop2.herokuapp.com/darkmode/1')
+                    .catch(error => console.log(error));
+            }
         }
         else{
             // Quitar el css que vuelve oscuro la página
-            let links  = Array.from(document.getElementsByTagName('link'));
-            let cssDarkMode = links.find(l => l.href.includes('/css/partials/darkmode.css'));
-            head.removeChild(cssDarkMode);
-            logo.src = '/images/logo.png';
-            search.src = '/images/icons/search.png';
+            if(cssDarkMode){
+                head.removeChild(cssDarkMode);
+                logo.src = '/images/logo.png';
+                search.src = '/images/icons/search.png';
 
-            // Mandar la información al servidor
-            fetch('https://tecnoshop2.herokuapp.com/darkmode/0')
-                .catch(error => console.log(error));
+                // Mandar la información al servidor
+                fetch('https://tecnoshop2.herokuapp.com/darkmode/0')
+                    .catch(error => console.log(error));
+            }
         }
     });
 });
